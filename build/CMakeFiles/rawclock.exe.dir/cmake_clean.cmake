226226e01ca21be0c205e57c21da9874cc8480c7
joemowed <maloney.jg2@gmail.com>
2024-10-17 00:24:31 -0400

    working the problem

---
+++ build/CMakeFiles/rawclock.exe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rawclock.exe.dir/samd51/linker/startup_samd51.c.o"
+  "CMakeFiles/rawclock.exe.dir/samd51/linker/startup_samd51.c.o.d"
+  "CMakeFiles/rawclock.exe.dir/src/main.cpp.o"
+  "CMakeFiles/rawclock.exe.dir/src/main.cpp.o.d"
+  "rawclock.exe"
+  "rawclock.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/rawclock.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/rawclock.exe.dir/src/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/rawclock.exe.dir/src/main.cpp.o: \
+ /home/jmaloney/Documents/rawClock/src/main.cpp
