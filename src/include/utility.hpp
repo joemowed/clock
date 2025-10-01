bf14c5f88526beb5fa8e49d700b890048d7bc65f
Joe <maloney.jg2@gmail.com>
2025-10-01 09:37:35 -0400

    added util functions

--- src/include/utility.hpp
+++ src/include/utility.hpp
@@ -1,3 +1,35 @@
 #include "samd51j20a.h"
+namespace util {
 inline port_group_registers_t &port_a = PORT_REGS->GROUP[0];
 inline port_group_registers_t &port_b = PORT_REGS->GROUP[1];
+
+inline uint32_t generateMask(const uint32_t length) {
+    uint32_t generated_msk = 0;
+    for (uint32_t i = 0; i < length; i++) {
+        generated_msk |= 1 << i;
+    }
+    return generated_msk;
+}
+
+inline void pulse_delay_loop() {
+
+    constexpr const uint32_t clock_loop_max = 0x02;
+    for (volatile uint32_t i = 0; i < clock_loop_max; i += 1)
+        ;
+}
+
+typedef struct RegBits {
+    uint32_t set;
+    uint32_t clear;
+} RegBits;
+inline RegBits generate_reg_bits(const uint32_t value, const uint32_t offset, const uint32_t length) {
+
+    uint32_t generated_msk = util::generateMask(length);
+
+    const uint32_t address_msk = (generated_msk << offset);
+
+    const uint32_t set_bits = value << offset;
+    const uint32_t clear_bits = (~set_bits) & address_msk;
+    return {set_bits, clear_bits};
+}
+} // namespace util
