a1856ad8421c27320f17811dfacd0eadf7fc399d
Joe Maloney <maloney.jg2@gmail.com>
2025-10-06 22:56:10 -0400

    character display working with old font

--- src/font.cpp
+++ src/font.cpp
@@ -1,25 +1,16 @@
 #include "font.hpp"
 #include <array>
-std::array<Font::DecompChar, 256> Font::decompressed_font_array = {};
-void Font::init() {
-    for (int curr_char = '!'; curr_char < 256; curr_char++) {
-        decompressed_font_array.at(curr_char) = decompress(curr_char);
-    }
-}
-Font::DecompChar Font::decompress(uint8_t character) {
-    DecompChar ret = {{}};
-    const int row_offset = (character) * 5;
-    for (int curr_col = 0; curr_col < 5; curr_col++) {
-        const uint8_t compressed_col = COMPRESSED_FONT_ARRAY.at(row_offset + curr_col);
-
-        for (int curr_row = 0; curr_row < 7; curr_row++) {
-
-            volatile auto temp = ret.at(curr_row);
-            temp |= compressed_col & (1 << curr_col);
-
-            ret.at(curr_row) = temp;
+Font::DecompChar Font::getChar(uint8_t chararcter) {
+    Font::DecompChar ret = {};
+    uint32_t comp_offset = (chararcter - START_OFFSET) * 5;
+    for (int x = 0; x < 5; x++) {
+        const uint8_t curr_col = COMPRESSED_FONT_ARRAY.at(comp_offset + x);
+        for (int y = 0; y < 8; y++) {
+            uint8_t temp = curr_col & (1 << y);
+            temp = (temp > 0);
+            temp <<= x;
+            ret.at(y) |= temp;
         }
     }
     return ret;
 }
-Font::DecompChar Font::getChar(uint8_t chararcter) { return decompressed_font_array.at(chararcter); }
--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,24 @@ void loadLouie(const int8_t x_offset, const int8_t y_offset) {
         }
     }
 }
+void drawChar(const uint8_t character, const uint8_t color) {
+    Font::DecompChar curr_char = Font::getChar(character);
+    for (int y = 0; y < 8; y++) {
+        const uint8_t curr_row = curr_char.at(y);
+        for (int x = 0; x < 5; x++) {
+            if (curr_row & (1 << x)) {
+                Display::updatePixel(color, x, y);
+
+            } else {
+                Display::updatePixel(0x0, x, y);
+            }
+        }
+    }
+}
 int main(void) {
-    Font::init();
     Display::init();
     Display::fill(0x7);
-    for (uint16_t i = ' '; i < 256; i++) {
-        auto curr_char = Font::getChar(i);
-        volatile auto i2 = curr_char;
-    }
+    drawChar('A', 0x2);
     loadLouie(32, -2);
 
     while (true) {
