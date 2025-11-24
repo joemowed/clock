bb846b6041bfc2b1b911edf5cb2f0090be5e8057
Joe Maloney <maloney.jg2@gmail.com>
2025-11-24 16:05:43 -0500

    create new scripts for building and uploading in debug mode

---
+++ cmake/debug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+mkdir -p ../build
+cd ../build  
+cmake ../cmake #-DCMAKE_BUILD_TYPE=RELEASE
+#ln -s cmake/compile_commands.json ../   > /dev/null 2>&1 
+make
+
+#ln -s cmake/compile_commands.json ../   > /dev/null 2>&1 
+make
+cd ../cmake/
+JLinkExe -commanderscript flash_debug.jlink
